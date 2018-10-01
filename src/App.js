9b1510c749529e4df72eac6b852e2dc42770bb8b
Tyler <tyler.roberts93@outlook.comgit config --global user.email tyler.roberts93@outlook.com>
2018-10-01 16:40:19 -0500

    Got HomePage to show content

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
+import { Route } from "react-router-dom"
+import HomePage from './components/HomePage.js'
+
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
 
   render() {
+    return(
+      <div>
+
+      <Route exact path='/' component={ HomePage } />
+
+      </div>
+    );
+
+
+
+
     return (
       <div className="app">
         {this.state.showSearchPage ? (
